82a8b08eac89bb55db5b0643bf52da3ca1d20ba1
CodeSevenic <sevenicbookings@gmail.com>
2022-12-26 16:50:15 +0200

    React Redux Code Update

---
+++ src/components/Navbar.js
@@ -0,0 +1,6 @@
+﻿import { CartIcon } from '../icons';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+  return <nav className="nav-center">Navbar</nav>;
+};
--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 ﻿import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartReducer, 
+  },
 });
