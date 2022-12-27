ac3ad7ceb169cc9c107bafff6a68d64693b8d4c0
CodeSevenic <sevenicbookings@gmail.com>
2022-12-27 19:51:01 +0200

    React Redux Code Update

--- src/features/cart/modal/modalSlice.js
+++ src/features/cart/modal/modalSlice.js
@@ -3,3 +3,13 @@
 const initialState = {
   isOpen: false,
 };
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    openModal: (state, action) => {
+      state.isOpen = true;
+    },
+  },
+});
