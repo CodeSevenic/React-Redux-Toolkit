554e7f57a779024866ed5fcae0bc8854962221e6
CodeSevenic <sevenicbookings@gmail.com>
2022-12-27 07:15:39 +0200

    React Redux Code Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { calculateTotals } from './features/cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
